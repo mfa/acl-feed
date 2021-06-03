0f662704ab0f20f2d3c989b786181c5b14ee9b8a
Andreas Madsack <andreas@madflex.de>
2021-06-04 00:49:41 +0200

    merge tests to one testfile

--- app/conftest.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-import pytest
-
-
-@pytest.fixture
-def example_html():
-    fn = Path(__file__).parent.absolute() / "fixtures/andreas-madsack.html"
-    yield fn.open().read()
--- app/test_all.py
+++ app/test_all.py
@@ -1,4 +1,14 @@
+from pathlib import Path
+
+import pytest
+from generate import generate_feed
 from parse import parse_html
+
+
+@pytest.fixture
+def example_html():
+    fn = Path(__file__).parent.absolute() / "fixtures/andreas-madsack.html"
+    yield fn.open().read()
 
 
 def test_parse_html(example_html):
@@ -19,3 +29,10 @@ def test_parse_html(example_html):
     assert set(feed[0].get("authors")) == {"Andreas Madsack", "Robert Weißgraeber"}
     assert feed[0]["year"] == "2019"
     assert feed[0]["link"] == "/anthology/W19-4201/"
+
+
+def test_generate_feed(example_html):
+    feed_data = parse_html(example_html, "andreas-madsack")
+    result = generate_feed(feed_data)
+    # FIXME: not the best way to test this
+    assert len(result.decode()) == 4211
