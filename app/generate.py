7788779a73f62335c163ae867ac1dd3dc2e65342
Andreas Madsack <andreas@madflex.de>
2025-04-04 16:30:08 +0200

    improve feed valitidity
    example validation: https://validator.w3.org/feed/check.cgi?url=https%3A%2F%2Facl-feed.madflex.de%2Fjacob-devlin.atom

--- app/generate.py
+++ app/generate.py
@@ -9,20 +9,20 @@ def generate_feed(parsed_feed):
     fg.title(f"{person} (ACL Antology)")
     fg.author({"name": person})
     slug = parsed_feed["slug"]
-    fg.id(slug)
+    fg.id(f"https://aclanthology.org/people/{slug[0]}/{slug}/")
     fg.link(href=f"https://acl-feed.madflex.de/{slug}.atom", rel="self")
     fg.language("en")
     fg.generator(generator="acl-feed", uri="https://acl-feed.madflex.de", version="1")
 
     for item in reversed(parsed_feed.get("feed", [])):
         fe = fg.add_entry()
-        fe.id(item["link"].split("/")[-2])
+        fe.id("https://aclanthology.org/" + item["link"].split("/")[-2])
         fe.title(item["title"])
         fe.link(href="https://aclanthology.org" + item["link"])
         fe.author({"name": ", ".join(item["authors"])})
         content = "<p>In: " + item["volume"] + "</p>"
         if "abstract" in item:
             content += item["abstract"]
-        fe.content(content)
+        fe.content(content, type="html")
 
     return fg.atom_str()
